f0942755a9b0b79e4b1101e25fa529c793cc099a
Corban Swain <CorbanSwain@gmail.com>
2018-02-28 09:16:28 -0500

    workin version for part 2

--- die_bootstrap.m
+++ die_bootstrap.m
@@ -6,18 +6,27 @@ numRolls = length(sample);
 numRepeats = 10 .^ (1:4);
 
 %% Functions
-   function X = simDieRoll(numRolls, numRepeats)
-      X = ceil(rand(numRolls, numRepeats) * 6);
+   function X = simResample(numRolls, numRepeats)
+      X = sample(ceil(rand(numRolls, numRepeats) * numRolls));
    end
 
 %%
+CNSUtils.FigureBuilder.setDefaults();
 lenNumRepeats = length(numRepeats);
 [meanCache, stdevCache] = deal(zeros(lenNumRepeats, 1));
+figure(1);
 for iNumRepeats = 1:lenNumRepeats
-   S = simDieRoll(numRolls, numRepeats(iNumRepeats));
+   m = numRepeats(iNumRepeats);
+   S = simResample(numRolls, m);
    medianDistribution = factorial(median(S, 1));
-   figure;
-   histogram(medianDistribution, 720);
+   subplot(4, 1, iNumRepeats);
+   nbins = 25;
+   %edges = ((1:20) - 1).*(721 / (nbins - 1));
+   edges = [0, factorial(1:4) + 0.5];
+   histogram(medianDistribution, 'Normalization', 'probability');
+   box('off');
+   ylim([0 0.55]);
+   xlim([0 25]);
    meanCache(iNumRepeats) = mean(medianDistribution);
    stdevCache(iNumRepeats) = std(medianDistribution);  
 end
